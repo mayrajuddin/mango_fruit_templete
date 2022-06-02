e90b5a25b92241b652b77315962530b91aca77b2
uddinmayraj <mayrajuddin03@gmail.com>
2022-06-02 20:52:19 +0600

    add

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(function(){
-    
+    $(window).on('scroll', function(){
+        var menuHeight = $('#main_menu').height();
+        var scrollSize = $(window).scrollTop();
+        if(scrollSize > menuHeight){
+            $('#main_menu').addClass('animate');
+        }else{
+            $('#main_menu').removeClass('animate');
+        }
+    })
 })
