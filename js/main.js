bc85072435f5a7a921c8631cb48fe91057328f53
uddinmayraj <mayrajuddin03@gmail.com>
2022-06-03 10:17:38 +0600

    add

--- js/main.js
+++ js/main.js
@@ -8,4 +8,24 @@ $(function(){
             $('#main_menu').removeClass('animate');
         }
     })
+    $('.product_slider').slick({
+        slidesToShow: 4,
+        prevArrow : $('.prvarrow'),
+        nextArrow: $('.nxtarrow')
+    })
+    $('.p_product_slider').slick({
+        slidesToShow: 5,
+        prevArrow : $('.p_prvarrow'),
+        nextArrow: $('.p_nxtarrow')
+    })
+    $('.week_slider').slick({
+        slidesToShow: 4,
+        prevArrow : $('.w_prvarrow'),
+        nextArrow: $('.w_nxtarrow')
+    })
+    $('.spcialC_slider').slick({
+        slidesToShow: 4,
+        prevArrow : $('.s_prvarrow'),
+        nextArrow: $('.s_nxtarrow')
+    })
 })
