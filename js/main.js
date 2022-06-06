00a2c213a52a976989a9f73ed84786631aebe54d
uddinmayraj <mayrajuddin03@gmail.com>
2022-06-06 22:47:36 +0600

    add

--- js/main.js
+++ js/main.js
@@ -17,21 +17,62 @@ $(function(){
     $('.product_slider').slick({
         slidesToShow: 4,
         prevArrow : $('.prvarrow'),
-        nextArrow: $('.nxtarrow')
+        nextArrow: $('.nxtarrow'),
+        responsive: [
+            {
+              breakpoint: 769,
+              settings: {
+                slidesToShow: 3
+              }
+            },
+            // {
+            //   breakpoint: 480,
+            //   settings: {
+            //     arrows: false,
+            //     centerMode: true,
+            //     centerPadding: '40px',
+            //     slidesToShow: 1
+            //   }
+            // }
+        ]
     })
     $('.p_product_slider').slick({
         slidesToShow: 5,
         prevArrow : $('.p_prvarrow'),
-        nextArrow: $('.p_nxtarrow')
+        nextArrow: $('.p_nxtarrow'),
+        responsive: [
+            {
+              breakpoint: 769,
+              settings: {
+                slidesToShow: 3
+              }
+            }
+        ]
     })
     $('.week_slider').slick({
         slidesToShow: 4,
         prevArrow : $('.w_prvarrow'),
-        nextArrow: $('.w_nxtarrow')
+        nextArrow: $('.w_nxtarrow'),
+        responsive: [
+            {
+              breakpoint: 769,
+              settings: {
+                slidesToShow: 3
+              }
+            }
+        ]
     })
     $('.spcialC_slider').slick({
         slidesToShow: 4,
         prevArrow : $('.s_prvarrow'),
-        nextArrow: $('.s_nxtarrow')
+        nextArrow: $('.s_nxtarrow'),
+        responsive: [
+            {
+              breakpoint: 769,
+              settings: {
+                slidesToShow: 3
+              }
+            }
+        ]
     })
 })
